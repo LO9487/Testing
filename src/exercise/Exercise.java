ae6475e7faf2cfadc833da7a921168fcf95e8204
Lo <Lo@LAPTOP-8060IC8J>
2023-11-02 13:50:11 +0800

    First Commit

---
+++ src/exercise/Exercise.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercise;
+
+/**
+ *
+ * @author Lo
+ */
+import java.util.Scanner;
+import java.util.Random;
+
+public class Exercise {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x=9;
+        int y = 10;
+        
+if ( x < 10) {
+if ( y < 10)
+System.out.println("*****");
+System.out.println("#####");
+}
+else {
+System.out.println("$$$$$");
+}
+
+//    int[] RandomA = new int[10];
+//        
+//    Random random = new Random();
+//        for (int i=0;i<10; i++){
+//            RandomA[i] = random.nextInt(101);
+//            System.out.print(RandomA[i]+" ");
+//        }
+//    int min = RandomA[0];
+//for(int i=0; i<RandomA.length; i++){   
+//        if (RandomA[i]< min){ 
+//              RandomA[i]= min;
+//           
+//    
+//}
+//} System.out.println("\n The min number is"+min);
+    }}
